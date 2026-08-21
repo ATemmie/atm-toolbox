02163616aa5a04e85c03811f67065508fd6b6b92
ATemmie <atemmie@users.noreply.github.com>
2026-08-21 18:07:13 +0800

    usage auto 20260821_180713

--- _run_key_task.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-schtasks /Delete /TN AddLaptopKey /F 2>nul
-schtasks /Create /TN AddLaptopKey /TR "powershell -ExecutionPolicy Bypass -File C:\add_laptop_key.ps1" /SC ONCE /ST 00:00 /RL HIGHEST /F /RU SYSTEM
-schtasks /Run /TN AddLaptopKey
-timeout /t 5 /nobreak >nul
-schtasks /Query /TN AddLaptopKey /FO LIST 2>nul
-echo DONE
