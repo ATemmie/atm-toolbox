5857bc26af923a2fd9f9a824d91eaaf4a188f2b4
ATemmie <atemmie@users.noreply.github.com>
2026-08-21 15:16:11 +0800

    usage auto 20260821_151611

---
+++ _addkey.ps1
@@ -0,0 +1,10 @@
+$key = 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMPJh4wzjUP8YZZIeMf9wOVCXKLNTC647b/6NLjVK3sl atemmie@WIN-8ITJT0L7H4P'
+$authFile = 'C:\Users\34934.ATEMMIE\.ssh\authorized_keys'
+$content = Get-Content $authFile -Raw
+if ($content -like '*atemmie@WIN-8ITJT0L7H4P*') {
+    "ALREADY_PRESENT"
+} else {
+    Add-Content -Path $authFile -Value $key
+    "APPENDED"
+}
+"KEY_COUNT: " + (Get-Content $authFile | Measure-Object -Line).Lines
